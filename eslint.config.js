e087b6dcc588969945beb5f22ef6752e0ffc6cd2
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-05-17 09:32:40 +0300

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,79 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
+import vitest from "@vitest/eslint-plugin";
+import eslintConfigPrettier from "eslint-config-prettier/flat";
+import { importX } from "eslint-plugin-import-x";
+import { globalIgnores, defineConfig } from "eslint/config";
+
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.strictTypeChecked,
+      tseslint.configs.stylisticTypeChecked,
+      importX.flatConfigs.recommended,
+      importX.flatConfigs.typescript,
+      eslintConfigPrettier,
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.vitest,
+      },
+      ecmaVersion: "latest",
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    rules: {
+      "import-x/order": [
+        "error",
+        {
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "index",
+            "sibling",
+            "parent",
+            "object",
+            "type",
+          ],
+        },
+      ],
+      "import-x/no-cycle": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/restrict-template-expressions": [
+        "error",
+        { allowNumber: true },
+      ],
+      "@typescript-eslint/no-empty-object-type": [
+        "error",
+        { allowInterfaces: "with-single-extends" },
+      ],
+    },
+  },
+  {
+    files: ["**/*.test.{ts,tsx}"],
+    extends: [vitest.configs.all],
+    rules: {
+      "@typescript-eslint/unbound-method": "off",
+      "vitest/require-hook": ["error", { allowedFunctionCalls: ["app.use"] }],
+      "vitest/no-hooks": "off",
+    },
+    settings: {
+      vitest: {
+        typecheck: true,
+      },
+    },
+  },
+]);
