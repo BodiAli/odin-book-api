65bfe18060abed68086f52c9dc996dbedfbafa2e
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-04 01:05:44 +0300

    Feat: use openapiRouter for /api-docs path

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import "dotenv/config";
 import express from "express";
 import indexRouter from "./routes/index-router.js";
+import openapiRouter from "./routes/openapi-router.js";
 
 const app = express();
 
 app.use("/", indexRouter);
-// app.use("/api-docs", docsRouter)
+app.use("/api-docs", openapiRouter);
 
 const PORT = Number(process.env.PORT) || 3000;
 
