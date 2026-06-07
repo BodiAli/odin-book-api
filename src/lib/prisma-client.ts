1538a2e8e17060de733a5455250990b3555d483c
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-07 06:59:02 +0300

    Feat: hide sensitive data from being accidentally exposed

--- src/lib/prisma-client.ts
+++ src/lib/prisma-client.ts
@@ -8,6 +8,13 @@ const connectionString =
     : process.env.DATABASE_URL;
 
 const adapter = new PrismaPg({ connectionString });
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient({
+  adapter,
+  omit: {
+    user: {
+      password: true,
+    },
+  },
+});
 
 export default prisma;
