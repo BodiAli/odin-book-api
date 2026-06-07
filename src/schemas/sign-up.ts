88ab189ccbaedf8a4c50fa45567bc52df0e6ccfb
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-07 07:00:00 +0300

    Feat: inline export for more consistent approach

--- src/schemas/sign-up.ts
+++ src/schemas/sign-up.ts
@@ -1,17 +1,15 @@
 import { z } from "zod";
 import { UserSchema } from "./user-schema.js";
 
-const RequestBody = z
+export const RequestBody = z
   .object({
     email: z.email(),
   })
   .openapi("SignUpRequestBody");
 
-const ResponseBody = z
+export const ResponseBody = z
   .object({
     token: z.jwt(),
     user: UserSchema,
   })
   .openapi("SignUpResponseBody");
-
-export { RequestBody, ResponseBody };
