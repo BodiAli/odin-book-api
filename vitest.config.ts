c5067646083d08ffb7f38131dfee6a97f329eb07
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-08 09:58:08 +0300

    Fix: use absolute path as replacement to alias
    when using relative path vi.mock doesn’t work as expected

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,3 +1,4 @@
+import path from "node:path";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
@@ -5,6 +6,8 @@ export default defineConfig({
     globals: true,
     fileParallelism: false,
     setupFiles: "./src/tests/setup/setup.ts",
-    alias: [{ find: "#src", replacement: "./src" }],
+    alias: [
+      { find: "#src", replacement: path.resolve(import.meta.dirname, "src") },
+    ],
   },
 });
