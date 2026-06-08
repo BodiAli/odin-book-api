173e2e541a42c60b8e09e0f7b22180d86e55376f
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-08 08:29:49 +0300

    Feat: use error handler to catch errors thrown in all endpoints

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import "dotenv/config";
 import express from "express";
 import indexRouter from "./routes/index-router.js";
 import openapiRouter from "./routes/openapi-router.js";
+import catchAllErrorHandler from "./middlewares/catch-all-error.js";
 
 const app = express();
 
 app.use("/", indexRouter);
 app.use("/api-docs", openapiRouter);
+app.use(catchAllErrorHandler);
 
 const PORT = Number(process.env.PORT) || 3000;
 
