0a97256127a37859aa2806ad0a80785f98db205e
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-08 07:57:16 +0300

    Feat: implement tests for catch all middleware

---
+++ src/tests/unit/middlewares/catch-all-error.test.ts
@@ -0,0 +1,55 @@
+import supertest from "supertest";
+import express from "express";
+import { afterEach, beforeEach, describe, expect, it, vi } from "vitest";
+import errorHandler from "#src/middlewares/catch-all-error.js";
+import * as errorSchemas from "#src/schemas/error-schemas.js";
+import type { z } from "zod";
+
+const app = express();
+
+describe("catch all error handler", () => {
+  beforeEach(() => {
+    app.get("/test", (_req, _res, next) => {
+      next(new Error("test: error thrown"));
+    });
+    app.use(errorHandler);
+    vi.spyOn(console, "error").mockImplementation(() => null);
+  });
+
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+
+  it("should call console.error", async () => {
+    expect.hasAssertions();
+
+    const spyConsoleError = vi.spyOn(console, "error");
+
+    await supertest(app).get("/test");
+
+    expect(spyConsoleError).toHaveBeenCalledExactlyOnceWith(
+      new Error("test: error thrown"),
+    );
+  });
+
+  it("should return a 500 status code", async () => {
+    expect.hasAssertions();
+
+    const response = await supertest(app).get("/test");
+
+    expect(response.statusCode).toBe(500);
+  });
+
+  it("should return a response body with the error message", async () => {
+    expect.hasAssertions();
+
+    const response = await supertest(app).get("/test");
+    const typedResponseBody = response.body as z.infer<
+      (typeof errorSchemas)["ServerError"]
+    >;
+
+    expect(typedResponseBody).toStrictEqual<typeof typedResponseBody>({
+      error: "test: error thrown",
+    });
+  });
+});
