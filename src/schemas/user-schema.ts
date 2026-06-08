0f30f7e31102bd580410cf38c540a5f5686503e7
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-08 07:15:24 +0300

    Feat: add username and fullName fields

--- src/schemas/user-schema.ts
+++ src/schemas/user-schema.ts
@@ -1,11 +1,11 @@
 import { z } from "zod";
 import type { UserModel } from "#src/generated/prisma/models.js";
 
-const UserSchema: z.ZodType<UserModel> = z
+export const UserSchema: z.ZodType<Omit<UserModel, "password">> = z
   .object({
     id: z.uuid(),
     email: z.email(),
+    username: z.string(),
+    fullName: z.string(),
   })
   .openapi("UserSchema");
-
-export { UserSchema };
