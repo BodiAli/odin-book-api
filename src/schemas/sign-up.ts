3e64fadbd554548357adc15ebbdf8b10c4bc14ab
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-08 07:14:48 +0300

    Feat: implement request body expected fields with expected valid formats

--- src/schemas/sign-up.ts
+++ src/schemas/sign-up.ts
@@ -3,7 +3,28 @@ import { UserSchema } from "./user-schema.js";
 
 export const RequestBody = z
   .object({
-    email: z.email(),
+    email: z
+      .email("Please provide a valid Email.")
+      .max(254, "Email cannot exceed 254 characters."),
+    username: z
+      .string("Please provide a string Username.")
+      .trim()
+      .nonempty("Username cannot be empty.")
+      .regex(/^\S+$/, "Username cannot include space.")
+      .max(100, "Username cannot exceed 100 characters."),
+    fullName: z
+      .string("Please provide a string Full Name.")
+      .trim()
+      .nonempty("Full Name cannot be empty.")
+      .max(100, "Username cannot exceed 100 characters."),
+    password: z
+      .string("Please provide a string Password.")
+      .min(5, "Password must be at least 5 characters."),
+    confirmPassword: z.string("Please provide a string Confirm Password."),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    error: "Passwords do not match.",
   })
   .openapi("SignUpRequestBody");
 
