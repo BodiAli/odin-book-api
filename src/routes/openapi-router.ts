f61648acf984f8a1d5da7b7be99a187b4f963bf1
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-09 06:42:00 +0300

    Feat: serve openapi.json file

--- src/routes/openapi-router.ts
+++ src/routes/openapi-router.ts
@@ -1,3 +1,4 @@
+import path from "node:path";
 import { Router } from "express";
 import * as swaggerUi from "swagger-ui-express";
 import openapiDocument from "#src/generated/openapi/openapi.json" with { type: "json" };
@@ -6,5 +7,10 @@ const openapiRouter = Router();
 
 openapiRouter.use("/", swaggerUi.serve);
 openapiRouter.get("/", swaggerUi.setup(openapiDocument));
+openapiRouter.get("/openapi.json", (_req, res) => {
+  res.sendFile(
+    path.resolve(import.meta.dirname, "../generated/openapi/openapi.json"),
+  );
+});
 
 export default openapiRouter;
