0e8f831fb89639b3b14989930360f66c30d26607
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-09 06:14:55 +0300

    Refactor: use parse instead of safeParse

--- src/middlewares/validate-body.ts
+++ src/middlewares/validate-body.ts
@@ -1,22 +1,24 @@
+import { ZodError, type z } from "zod";
 import type { ClientError } from "#src/schemas/error-schemas.js";
 import type { NextFunction, Request, Response } from "express";
-import type { z } from "zod";
 
 export default function validateBody(zodSchema: z.ZodType) {
   return (req: Request, res: Response, next: NextFunction) => {
-    const result = zodSchema.safeParse(req.body);
-    if (result.error) {
-      const errorObject: ClientError = {
-        errors: result.error.issues.map((error) => {
-          return {
-            message: error.message,
-          };
-        }),
-      };
-      res.status(400).json(errorObject);
-      return;
+    try {
+      zodSchema.parse(req.body);
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        const errorObject: ClientError = {
+          errors: error.issues.map((error) => {
+            return {
+              message: error.message,
+            };
+          }),
+        };
+        res.status(400).json(errorObject);
+      }
+      next(error);
     }
-
-    next();
   };
 }
