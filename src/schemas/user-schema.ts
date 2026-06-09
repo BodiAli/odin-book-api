61d115e8cd0437ac6ab3554049e7b76dd98cd863
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-09 06:05:13 +0300

    Feat: create typescript type from zod schema

--- src/schemas/user-schema.ts
+++ src/schemas/user-schema.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
 import type { UserModel } from "#src/generated/prisma/models.js";
 
-export const UserSchema: z.ZodType<Omit<UserModel, "password">> = z.object({
+export const user: z.ZodType<Omit<UserModel, "password">> = z.object({
   id: z.uuid(),
   email: z.email(),
   username: z.string(),
   fullName: z.string(),
 });
+
+export type User = z.infer<typeof user>;
