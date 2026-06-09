2cd0e558e322382ea77d452e051a089ee7eb7c19
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-09 06:06:44 +0300

    Feat: implement username property test to validate that the middleware will return different error types based on the provided schema && test successful logic

--- src/tests/unit/middlewares/validate-body.test.ts
+++ src/tests/unit/middlewares/validate-body.test.ts
@@ -3,12 +3,12 @@ import { z } from "zod";
 import request from "supertest";
 import express from "express";
 import validateBody from "#src/middlewares/validate-body.js";
-import * as errorSchemas from "#src/schemas/error-schemas.js";
-
-const app = express();
-app.use(express.json());
+import type { ClientError } from "#src/schemas/error-schemas.js";
 
 describe("validate body middleware", () => {
+  const app = express();
+  app.use(express.json());
+
   beforeAll(() => {
     app.post(
       "/test",
@@ -40,15 +40,39 @@ describe("validate body middleware", () => {
 
     const response = await request(app).post("/test").send({});
 
-    expect(response.badRequest).toBe(true);
-    expect(response.body).toStrictEqual<
-      z.infer<(typeof errorSchemas)["ClientError"]>
-    >({
+    expect(response.body).toStrictEqual<ClientError>({
       errors: [
         {
           message: "Please provide a string username.",
         },
       ],
     });
+  });
+
+  it("should return 'Username cannot exceed 4 characters' when req.body.username is more than 4 characters", async () => {
+    expect.hasAssertions();
+
+    const response = await request(app)
+      .post("/test")
+      .send({ username: "test-username" });
+
+    expect(response.body).toStrictEqual<ClientError>({
+      errors: [
+        {
+          message: "Username cannot exceed 4 characters.",
+        },
+      ],
+    });
+  });
+
+  it("should call the next request handler when req.body matches the schema", async () => {
+    expect.hasAssertions();
+
+    const response = await request(app)
+      .post("/test")
+      .send({ username: "test" });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toBe("Passed");
   });
 });
