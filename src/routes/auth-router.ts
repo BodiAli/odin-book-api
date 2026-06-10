b049b2c2b3774fb83ce4d8d58cf32345cc77f019
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-10 03:44:46 +0300

    Feat: pass validateBody middleware to validate the request body of the POST request

--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
+import validateBody from "#src/middlewares/validate-body.js";
+import { signUpRequestBody } from "#src/schemas/sign-up.js";
+import * as authController from "#src/controllers/auth-controller.js";
 
 const authRouter = Router();
 
-authRouter.post("/sign-up", (_req, res) => {
+authRouter.post("/sign-up", validateBody(signUpRequestBody), (_req, res) => {
   res.json("HII");
 });
 
