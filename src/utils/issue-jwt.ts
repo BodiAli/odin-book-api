3daf00d53b1fb103e28ddeddd9981b3a661efd48
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-10 07:41:20 +0300

    Feat: add a default value for the expiresIn parameter

--- src/utils/issue-jwt.ts
+++ src/utils/issue-jwt.ts
@@ -1,7 +1,7 @@
 import * as jwt from "jsonwebtoken";
 import type { StringValue } from "ms";
 
-export default function issueJwt(sub: string, expiresIn: StringValue) {
+export default function issueJwt(sub: string, expiresIn: StringValue = "2w") {
   const token = jwt.sign({ sub }, process.env.JWT_SECRET, {
     expiresIn,
   });
