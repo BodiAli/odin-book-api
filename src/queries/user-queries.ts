042171ad06ab3c688e87f7025eff1b2f36cc64cb
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-10 03:34:25 +0300

    Feat: create function to create a new user

--- src/queries/user-queries.ts
+++ src/queries/user-queries.ts
@@ -1,4 +1,6 @@
+import * as bcrypt from "bcrypt";
 import prisma from "#src/lib/prisma-client.js";
+import type { SignUpRequestBody } from "#src/schemas/sign-up.js";
 
 export async function getUserWithPasswordByEmail(email: string) {
   const user = await prisma.user.findUnique({
@@ -6,6 +8,26 @@ export async function getUserWithPasswordByEmail(email: string) {
       email,
     },
     omit: { password: false },
+  });
+
+  return user;
+}
+
+export async function createUser({
+  email,
+  username,
+  fullName,
+  password,
+}: Omit<SignUpRequestBody, "confirmPassword">) {
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      username,
+      fullName,
+      password: hashedPassword,
+    },
   });
 
   return user;
