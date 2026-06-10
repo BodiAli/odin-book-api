02267cb32cbda4007438b8a370af7f2a14b2f189
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-10 07:39:29 +0300

    Feat: implement signing up user

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -1,12 +1,26 @@
 import * as userQueries from "#src/queries/user-queries.js";
-import type { SignUpRequestBody } from "#src/schemas/sign-up.js";
+import CustomHttpStatusError from "#src/errors/http-status-error.js";
+import issueJwt from "#src/utils/issue-jwt.js";
+import type {
+  SignUpRequestBody,
+  SignUpResponseBody,
+} from "#src/schemas/sign-up.js";
 import type { Request, Response } from "express";
+import type { ClientError } from "#src/schemas/error-schemas.js";
 
 export async function createUser(
-  req: Request<{}, {}, SignUpRequestBody>,
-  res: Response,
+  req: Request<unknown, unknown, SignUpRequestBody>,
+  res: Response<SignUpResponseBody | ClientError>,
 ) {
   const { confirmPassword: _, ...userData } = req.body;
-  const user = await userQueries.createUser(userData);
-  const jwtToken = issueJwt();
+
+  try {
+    const user = await userQueries.createUser(userData);
+    const jwtToken = issueJwt(user.id);
+    res.json({ token: jwtToken, user });
+  } catch (error) {
+    if (error instanceof CustomHttpStatusError) {
+      res.status(error.code).json({ errors: [{ message: error.message }] });
+    }
+  }
 }
