c7e7c5a4f8a4f7288bf119ef4c176310766c32e7
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-12 08:10:38 +0300

    Feat: create endpoint to handle POST requests

--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import validateBody from "#src/middlewares/validate-body.js";
 import { signUpRequestBody } from "#src/schemas/auth/sign-up.js";
 import * as authController from "#src/controllers/auth-controller.js";
+import { logInRequestBody } from "#src/schemas/auth/log-in.js";
 
 const authRouter = Router();
 
@@ -9,6 +10,11 @@ authRouter.post(
   "/sign-up",
   validateBody(signUpRequestBody),
   authController.createUser,
+);
+authRouter.post(
+  "/log-in",
+  validateBody(logInRequestBody),
+  authController.authenticateUser,
 );
 
 export default authRouter;
