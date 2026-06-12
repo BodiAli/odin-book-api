9ccd01763528db4793fdfc7eec53c19be499c34b
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-12 08:11:00 +0300

    Feat: create schemas for logIn endpoint

--- src/schemas/auth/log-in.ts
+++ src/schemas/auth/log-in.ts
@@ -1,1 +1,21 @@
-// export loginRequestBody =
+import { z } from "zod";
+import { user } from "../users/user-schema.js";
+
+export const logInRequestBody = z.object({
+  username: z
+    .string("Please provide a string Username.")
+    .trim()
+    .nonempty("Username cannot be empty.")
+    .regex(/^\S+$/, "Username cannot include space.")
+    .max(100, "Username cannot exceed 100 characters."),
+  password: z
+    .string("Please provide a string Password.")
+    .nonempty("Password cannot be empty."),
+});
+export const logInResponseBody = z.object({
+  token: z.string(),
+  user: user,
+});
+
+export type LogInRequestBody = z.infer<typeof logInRequestBody>;
+export type LogInResponseBody = z.infer<typeof logInResponseBody>;
