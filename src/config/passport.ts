5a30c4bf0b3fbf3aed994ee2900102fedd70318a
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-12 08:03:33 +0300

    Feat!: remove email field to only authenticate with username

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -4,33 +4,27 @@ import * as bcrypt from "bcrypt";
 import * as userQueries from "#src/queries/user-queries.js";
 
 passport.use(
-  new LocalStrategy(
-    { usernameField: "email", session: false },
-    (email, password, done) => {
-      const asyncHandler = async () => {
-        try {
-          const user = await userQueries.getUserWithPasswordByEmail(email);
-          if (!user) {
-            done(null, false, { message: "Incorrect email or password." });
-            return;
-          }
-
-          const isPasswordCorrect = await bcrypt.compare(
-            password,
-            user.password,
-          );
-          if (!isPasswordCorrect) {
-            done(null, false, { message: "Incorrect email or password." });
-            return;
-          }
-
-          done(null, user);
-        } catch (error) {
-          done(error);
+  new LocalStrategy({ session: false }, (username, password, done) => {
+    const asyncHandler = async () => {
+      try {
+        const user = await userQueries.getUserWithPasswordByUsername(username);
+        if (!user) {
+          done(null, false, { message: "Incorrect username or password." });
+          return;
         }
-      };
 
-      void asyncHandler();
-    },
-  ),
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) {
+          done(null, false, { message: "Incorrect username or password." });
+          return;
+        }
+
+        done(null, user);
+      } catch (error) {
+        done(error);
+      }
+    };
+
+    void asyncHandler();
+  }),
 );
