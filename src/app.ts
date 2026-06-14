bb841842920b2188411a67324a065dd63c3f87b5
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-14 06:20:40 +0300

    Feat: use resourceNotFound middleware to send a response when accessing an unknown resource

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import "dotenv/config";
 import express from "express";
+import resourceNotFound from "#src/middlewares/resource-not-found.js";
 import indexRouter from "./routes/index-router.js";
 import openapiRouter from "./routes/openapi-router.js";
-import catchAllErrorHandler from "./middlewares/catch-all-error.js";
+import genericErrorHandler from "./middlewares/generic-error-handler.js";
 
 const app = express();
 
 app.use("/", indexRouter);
 app.use("/api-docs", openapiRouter);
-app.use(catchAllErrorHandler);
+
+app.use(resourceNotFound);
+app.use(genericErrorHandler);
 
 const PORT = Number(process.env.PORT) || 3000;
 
