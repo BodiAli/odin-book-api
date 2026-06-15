bb7ede137aea8673fd4e577c5bc604f080608f9e
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-15 11:05:33 +0300

    Feat: initialize google consent and callback paths

--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -3,6 +3,7 @@ import validateBody from "#src/middlewares/validate-body.js";
 import { signUpRequestBody } from "#src/schemas/auth/sign-up.js";
 import * as authController from "#src/controllers/auth-controller.js";
 import { logInRequestBody } from "#src/schemas/auth/log-in.js";
+import passport from "passport";
 
 const authRouter = Router();
 
@@ -15,6 +16,18 @@ authRouter.post(
   "/log-in",
   validateBody(logInRequestBody),
   authController.authenticateUser,
+);
+
+authRouter.get("/google", passport.authenticate("google"), (req, res) => {
+  res.json("HELLO");
+});
+
+authRouter.get(
+  "/google/callback",
+  passport.authenticate("google", { session: false }),
+  (req, res) => {
+    res.json("HELLO2");
+  },
 );
 
 export default authRouter;
