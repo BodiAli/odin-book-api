ba858fe3e5c9ca836550def9c6941802347ebb44
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-15 11:05:04 +0300

    Feat: initialize GoogleStrategy

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,6 +1,7 @@
 import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
 import * as bcrypt from "bcrypt";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import * as userQueries from "#src/queries/user-queries.js";
 import type { User } from "#src/schemas/users/user-schema.js";
 
@@ -33,4 +34,21 @@ passport.use(
 
     void asyncHandler();
   }),
+);
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      scope: ["email", "profile"],
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("ACCESS TOKEN", accessToken);
+      console.log("REFRESH TOKEN", refreshToken);
+      console.log("PROFILE", profile);
+      console.log("DONE", done);
+    },
+  ),
 );
