5ea1b009879568d2353f8d2fb34f758eef01827a
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-18 14:49:39 +0300

    Fix: import jsonwebtoken as a default import because it’s a commonjs module with a .default property

--- src/utils/issue-jwt.ts
+++ src/utils/issue-jwt.ts
@@ -1,4 +1,7 @@
-import * as jwt from "jsonwebtoken";
+// module 'jsonwebtoken' is a CommonJS module, which may not support all module.exports as named exports.
+// CommonJS modules can always be imported via the default export
+// eslint-disable-next-line import-x/default
+import jwt from "jsonwebtoken";
 import type { StringValue } from "ms";
 
 export default function issueJwt(sub: string, expiresIn: StringValue = "2w") {
