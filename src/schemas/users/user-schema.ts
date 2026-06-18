13bb2f3617254c6bc4997264e9a4a9ec56027114
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-18 14:43:56 +0300

    Feat: add provider field

--- src/schemas/users/user-schema.ts
+++ src/schemas/users/user-schema.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { Provider } from "#src/generated/prisma/enums.js";
 import type { UserModel } from "#src/generated/prisma/models.js";
 
 export const user: z.ZodType<Omit<UserModel, "password">> = z.object({
@@ -6,6 +7,7 @@ export const user: z.ZodType<Omit<UserModel, "password">> = z.object({
   email: z.email(),
   username: z.string(),
   fullName: z.string(),
+  provider: z.enum(Provider),
 });
 
 export type User = z.infer<typeof user>;
