ccc1ce0ad6e71b3b2153db99e425774b26e774a6
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-18 16:35:36 +0300

    rest: create test for createProfile method

--- src/tests/unit/queries/profile-queries.test.ts
+++ src/tests/unit/queries/profile-queries.test.ts
@@ -1,2 +1,32 @@
-import { describe, it } from "vitest";
-import * as profileQueries from "#src/queries/";
+import { describe, expect, it } from "vitest";
+import * as profileQueries from "#src/queries/profile-queries.js";
+import prisma from "#src/lib/prisma-client.js";
+import type { ProfileModel } from "#src/generated/prisma/models.js";
+
+describe("profile-queries", () => {
+  describe(profileQueries.createProfile, () => {
+    it("should create a new profile", async () => {
+      expect.hasAssertions();
+
+      const createdUser = await prisma.user.create({
+        data: {
+          email: "test-email@test.com",
+          fullName: "test: full name",
+        },
+      });
+
+      const profile = await profileQueries.createProfile({
+        userId: createdUser.id,
+        imageUrl: "test-image-url",
+        description: "test: profile description",
+      });
+
+      expect(profile).toStrictEqual<ProfileModel>({
+        id: profile.id,
+        description: "test: profile description",
+        imageUrl: "test-image-url",
+        userId: createdUser.id,
+      });
+    });
+  });
+});
