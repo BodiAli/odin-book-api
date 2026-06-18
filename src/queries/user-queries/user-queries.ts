44d42ad2a99bb01ed291f75f2a19cf8deb1b25d5
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-18 14:42:03 +0300

    Refactor: move file inside user-queries directory

---
+++ src/queries/user-queries/user-queries.ts
@@ -0,0 +1,71 @@
+import * as bcrypt from "bcrypt";
+import prisma from "#src/lib/prisma-client.js";
+import { Prisma } from "#src/generated/prisma/client.js";
+import CustomHttpStatusError from "#src/errors/http-status-error.js";
+import type { CreateUserArguments } from "./user-queries-types.js";
+
+export async function getUserWithPasswordByEmail(email: string) {
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+    omit: { password: false },
+  });
+
+  return user;
+}
+
+export async function getUserById(id: string) {
+  const user = await prisma.user.findUnique({
+    where: {
+      id,
+    },
+  });
+
+  return user;
+}
+
+export async function createUser({
+  email,
+  fullName,
+  password,
+  provider = "local",
+}: CreateUserArguments) {
+  const hashedPassword = password ? await bcrypt.hash(password, 10) : null;
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email,
+        fullName,
+        provider,
+        password: hashedPassword,
+      },
+    });
+
+    return user;
+  } catch (error) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2002") {
+        const field = (
+          error.meta?.["driverAdapterError"] as {
+            cause: {
+              constraint: {
+                fields: string[];
+              };
+            };
+          }
+        ).cause.constraint.fields["0"];
+
+        if (field === "email") {
+          throw new CustomHttpStatusError(409, "Email already exists.");
+        }
+        if (field === "username") {
+          throw new CustomHttpStatusError(409, "Username already exists.");
+        }
+      }
+    }
+
+    throw error;
+  }
+}
