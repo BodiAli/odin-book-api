4f5f1de76e4ec1bdfe4fa56e512a29974e1ecc24
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-18 09:21:07 +0300

    Fix: throw error in end of catch block && import AuthenticatedResponse type

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -2,18 +2,15 @@ import passport from "passport";
 import * as userQueries from "#src/queries/user-queries.js";
 import CustomHttpStatusError from "#src/errors/http-status-error.js";
 import issueJwt from "#src/utils/issue-jwt.js";
-import type {
-  SignUpRequestBody,
-  SignUpResponseBody,
-} from "#src/schemas/auth/sign-up.js";
+import type { SignUpRequestBody } from "#src/schemas/auth/sign-up.js";
 import type { NextFunction, Request, RequestHandler, Response } from "express";
 import type { ClientError } from "#src/schemas/errors/error-schemas.js";
-import type { LogInResponseBody } from "#src/schemas/auth/log-in.js";
 import type { User } from "#src/schemas/users/user-schema.js";
+import type { AuthenticatedResponse } from "#src/schemas/auth/authenticated-response.js";
 
 export async function createUser(
   req: Request<unknown, unknown, SignUpRequestBody>,
-  res: Response<SignUpResponseBody | ClientError>,
+  res: Response<AuthenticatedResponse | ClientError>,
 ) {
   const { confirmPassword: _, ...userData } = req.body;
 
@@ -25,12 +22,13 @@ export async function createUser(
     if (error instanceof CustomHttpStatusError) {
       res.status(error.code).json({ errors: [{ message: error.message }] });
     }
+    throw error;
   }
 }
 
 export function authenticateUser(
   req: Request,
-  res: Response<LogInResponseBody | ClientError>,
+  res: Response<AuthenticatedResponse | ClientError>,
   next: NextFunction,
 ) {
   (
