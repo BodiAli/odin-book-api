84d205cb57309e11c6f2e12a45d72597b0fbc6d9
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-18 14:40:55 +0300

    Feat: handle google oauth logic

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,19 +1,27 @@
+import assert from "node:assert";
 import passport from "passport";
 import { Strategy as LocalStrategy } from "passport-local";
 import * as bcrypt from "bcrypt";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
-import * as userQueries from "#src/queries/user-queries.js";
+import * as userQueries from "#src/queries/user-queries/user-queries.js";
 import type { User } from "#src/schemas/users/user-schema.js";
+import type { CreateUserArguments } from "#src/queries/user-queries/user-queries-types.js";
 
 passport.use(
   new LocalStrategy(
     { session: false, usernameField: "email" },
     (email, password, done) => {
+      const message = "Incorrect email or password.";
       const asyncHandler = async () => {
         try {
           const user = await userQueries.getUserWithPasswordByEmail(email);
           if (!user) {
-            done(null, false, { message: "Incorrect email or password." });
+            done(null, false, { message });
+            return;
+          }
+
+          if (!user.password) {
+            done(null, false, { message });
             return;
           }
 
@@ -22,14 +30,14 @@ passport.use(
             user.password,
           );
           if (!isPasswordCorrect) {
-            done(null, false, { message: "Incorrect email or password." });
+            done(null, false, { message });
             return;
           }
 
           const authenticatedUser: User = {
             email: user.email,
             fullName: user.fullName,
-            username: user.username,
+            provider: user.provider,
             id: user.id,
           };
           done(null, authenticatedUser);
@@ -51,8 +59,34 @@ passport.use(
       callbackURL: process.env.GOOGLE_CALLBACK_URL,
       scope: ["email", "profile"],
     },
-    (accessToken, refreshToken, profile, done) => {
-      done(null, { id: profile.id });
+    (_accessToken, _refreshToken, profile, done) => {
+      const asyncHandler = async () => {
+        try {
+          const user = await userQueries.getUserById(profile.id);
+
+          if (!user) {
+            assert(profile._json.email, "_json.email is undefined");
+            assert(profile._json.name, "_json.name is undefined");
+
+            const userData: CreateUserArguments = {
+              email: profile._json.email,
+              fullName: profile._json.name,
+              provider: "google",
+              password: null,
+            };
+            const createdUser = await userQueries.createUser(userData);
+
+            done(null, createdUser);
+            return;
+          }
+
+          done(null, user);
+        } catch (error) {
+          done(error, false);
+        }
+      };
+
+      void asyncHandler();
     },
   ),
 );
