bbb8406f915707b8afa8b62e9610659ce13a7d00
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-06-19 15:05:16 +0300

    Feat: add a picture field

--- src/schemas/users/user-schema.ts
+++ src/schemas/users/user-schema.ts
@@ -2,12 +2,15 @@ import { z } from "zod";
 import { Provider } from "#src/generated/prisma/enums.js";
 import type { UserModel } from "#src/generated/prisma/models.js";
 
-export const user: z.ZodType<Omit<UserModel, "password">> = z.object({
+export const user: z.ZodType<
+  Omit<UserModel, "password"> & { picture: string | null }
+> = z.object({
   id: z.uuid(),
   email: z.email(),
   username: z.string(),
   fullName: z.string(),
   provider: z.enum(Provider),
+  picture: z.string(),
 });
 
 export type User = z.infer<typeof user>;
