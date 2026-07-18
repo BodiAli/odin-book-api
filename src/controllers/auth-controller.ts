7709902c3ad816c01d139ca2530d6cde189b6ae1
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-07-18 21:37:52 +0300

    Feat: initialize controller to authenticate with github && minor fixes

--- src/controllers/auth-controller.ts
+++ src/controllers/auth-controller.ts
@@ -3,8 +3,9 @@ import jwt from "jsonwebtoken";
 import * as userQueries from "#src/queries/user-queries.js";
 import CustomHttpStatusError from "#src/errors/http-status-error.js";
 import issueJwt from "#src/utils/issue-jwt.js";
-import { googleOauth2 } from "#src/services/google-oauth2-service.js";
-import { fetchWrapperOauth2 } from "#src/utils/oauth2-fetch-wrapper.js";
+import { googleOauth2 } from "#src/services/google-oauth2-authenticate.js";
+import { getUserInfoGoogle } from "#src/services/get-user-info-google.js";
+import { getUserInfoGithub } from "#src/services/get-user-info-github.js";
 import type {
   SignUpRequestBody,
   AuthenticatedResponse,
@@ -71,20 +72,7 @@ export async function authenticateWithGoogle(
     }
     const { code, codeVerifier } = req.body;
 
-    const data = await fetchWrapperOauth2(
-      "https://oauth2.googleapis.com/token",
-      {
-        method: "POST",
-        body: JSON.stringify({
-          code,
-          client_id: process.env.GOOGLE_CLIENT_ID,
-          client_secret: process.env.GOOGLE_CLIENT_SECRET,
-          redirect_uri: process.env.GOOGLE_CALLBACK_URL,
-          grant_type: "authorization_code",
-          code_verifier: codeVerifier,
-        }),
-      },
-    );
+    const data = await getUserInfoGoogle({ code, codeVerifier });
     const payload = jwt.decode(data.id_token) as Oauth2UserData;
     const user = await googleOauth2(payload);
     const jwtToken = issueJwt(payload.sub);
@@ -106,4 +94,15 @@ export async function authenticateWithGoogle(
     }
     next(error);
   }
+}
+
+export async function authenticateWithGithub(
+  req: Request<unknown, unknown, { code: string }>,
+  res: Response,
+) {
+  const { code } = req.body;
+
+  const data = await getUserInfoGithub({ code });
+
+  res.json("HIII");
 }
