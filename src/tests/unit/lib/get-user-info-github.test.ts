64fecafcab2401d8e0246060ef4e9a2f22edd359
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-07-19 11:10:40 +0300

    Test: update test name

--- src/tests/unit/lib/get-user-info-github.test.ts
+++ src/tests/unit/lib/get-user-info-github.test.ts
@@ -11,73 +11,77 @@ describe(getUserInfoGithub, () => {
     code: "authorization-code",
   };
 
-  describe("exchanging authorization code for an access token", () => {
-    /* 
+  it("should throw a CustomHttpStatusError with a 401 status code when 'success' argument is false", async () => {});
+
+  /* 
     Github returns a 200 status code regardless of request error so when it responds with
     a code that is not 200 it means that this error is unexpected.
    */
-    it("should throw a CustomHttpStatusError with a 502 status code if response is not ok", async () => {
-      expect.hasAssertions();
+  it("should throw a CustomHttpStatusError with a 502 status code if response is not ok", async () => {
+    expect.hasAssertions();
 
-      vi.spyOn(globalThis, "fetch").mockResolvedValue(
-        new Response(JSON.stringify({ error: "Unexpected Error" }), {
-          status: 500,
-        }),
-      );
+    vi.spyOn(globalThis, "fetch").mockResolvedValue(
+      new Response(JSON.stringify({ error: "Unexpected Error" }), {
+        status: 500,
+      }),
+    );
 
-      await expect(getUserInfoGithub(argumentsObj)).rejects.toThrow(
-        new CustomHttpStatusError(502, "Failed to authenticate with Github."),
-      );
-    });
-
-    it("should throw a CustomHttpStatusError with a 400 status code when the authorization code is invalid", async () => {
-      expect.hasAssertions();
-
-      expect.hasAssertions();
-
-      vi.spyOn(globalThis, "fetch").mockResolvedValue(
-        new Response(
-          JSON.stringify({
-            error: "bad_verification_code",
-            error_description: "The code passed is incorrect or expired.",
-          }),
-          {
-            status: 200,
-          },
-        ),
-      );
-
-      await expect(getUserInfoGithub(argumentsObj)).rejects.toThrow(
-        new CustomHttpStatusError(
-          400,
-          "The code passed is incorrect or expired.",
-        ),
-      );
-    });
-
-    it("should throw a CustomHttpStatusError with a 400 status code when the user has not verified their Github email", async () => {
-      expect.hasAssertions();
-
-      expect.hasAssertions();
-
-      vi.spyOn(globalThis, "fetch").mockResolvedValue(
-        new Response(
-          JSON.stringify({
-            error: "unverified_user_email",
-            error_description: "The user must have a verified primary email.",
-          }),
-          {
-            status: 200,
-          },
-        ),
-      );
-
-      await expect(getUserInfoGithub(argumentsObj)).rejects.toThrow(
-        new CustomHttpStatusError(
-          400,
-          "Please verify your Github email before authenticating with Github.",
-        ),
-      );
-    });
+    await expect(getUserInfoGithub(argumentsObj)).rejects.toThrow(
+      new CustomHttpStatusError(502, "Failed to authenticate with Github."),
+    );
   });
+
+  it("should throw a CustomHttpStatusError with a 400 status code when the authorization code is invalid", async () => {
+    expect.hasAssertions();
+
+    expect.hasAssertions();
+
+    vi.spyOn(globalThis, "fetch").mockResolvedValue(
+      new Response(
+        JSON.stringify({
+          error: "bad_verification_code",
+          error_description: "The code passed is incorrect or expired.",
+        }),
+        {
+          status: 200,
+        },
+      ),
+    );
+
+    await expect(getUserInfoGithub(argumentsObj)).rejects.toThrow(
+      new CustomHttpStatusError(
+        400,
+        "The code passed is incorrect or expired.",
+      ),
+    );
+  });
+
+  it("should throw a CustomHttpStatusError with a 400 status code when the user has not verified their Github email", async () => {
+    expect.hasAssertions();
+
+    expect.hasAssertions();
+
+    vi.spyOn(globalThis, "fetch").mockResolvedValue(
+      new Response(
+        JSON.stringify({
+          error: "unverified_user_email",
+          error_description: "The user must have a verified primary email.",
+        }),
+        {
+          status: 200,
+        },
+      ),
+    );
+
+    await expect(getUserInfoGithub(argumentsObj)).rejects.toThrow(
+      new CustomHttpStatusError(
+        400,
+        "Please verify your Github email before authenticating with Github.",
+      ),
+    );
+  });
+
+  it.todo(
+    "should throw a CustomHttpStatusError with a 401 status code when access token is missing or invalid",
+  );
 });
