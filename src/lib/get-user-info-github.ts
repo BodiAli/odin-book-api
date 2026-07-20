ba2d603348a0ff0af51fb987ec043244df69569d
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-07-20 18:58:58 +0300

    Feat: finish implementing getting github user info and primary email

--- src/lib/get-user-info-github.ts
+++ src/lib/get-user-info-github.ts
@@ -1,27 +1,37 @@
+import assert from "node:assert";
 import CustomHttpStatusError from "#src/errors/http-status-error.js";
-import type { Oauth2RequestBody } from "#src/types/auth.js";
+import type { Oauth2RequestBody, Oauth2UserData } from "#src/types/auth.js";
 
-interface ErrorResponse {
+interface CodeErrorResponse {
   error: string;
   error_description: string;
 }
-interface SuccessfulResponse {
+interface CodeSuccessfulResponse {
   access_token: string;
 }
 
-type GithubResponse = ErrorResponse | SuccessfulResponse;
+type CodeExchangeResponse = CodeErrorResponse | CodeSuccessfulResponse;
 
-interface GithubEndpointErrorResponse {
-  message: string;
-  status: string;
+interface UserInfoResponse {
+  id: number;
+  name: string;
+  avatar_url: string;
 }
 
-export async function getUserInfoGithub(requestBody: Oauth2RequestBody) {
+type UserEmailsResponse = {
+  email: string;
+  verified: boolean;
+  primary: boolean;
+}[];
+
+export async function getUserInfoGithub(
+  requestBody: Oauth2RequestBody,
+): Promise<Oauth2UserData> {
   if (!requestBody.success) {
     throw new CustomHttpStatusError(401, "Access denied");
   }
 
-  const { code } = requestBody;
+  const { code, codeVerifier } = requestBody;
   const codeExchangeResponse = await fetch(
     "https://github.com/login/oauth/access_token",
     {
@@ -30,6 +40,7 @@ export async function getUserInfoGithub(requestBody: Oauth2RequestBody) {
         client_id: process.env.GITHUB_CLIENT_ID,
         client_secret: process.env.GITHUB_CLIENT_SECRET,
         code,
+        code_verifier: codeVerifier,
       }),
       headers: {
         "Content-Type": "application/json",
@@ -37,16 +48,19 @@ export async function getUserInfoGithub(requestBody: Oauth2RequestBody) {
       },
     },
   );
-
-  /* 
-    Github returns a 200 status code regardless of request error so when it responds with
-    a status code that is not 200 it means that this error is unexpected.
-   */
   if (!codeExchangeResponse.ok) {
+    const codeExchangeErrorResponse =
+      (await codeExchangeResponse.json()) as CodeErrorResponse;
+    if (codeExchangeErrorResponse.error === "invalid_grant") {
+      throw new CustomHttpStatusError(
+        400,
+        codeExchangeErrorResponse.error_description,
+      );
+    }
     throw new CustomHttpStatusError(502, "Failed to authenticate with Github.");
   }
   const codeExchangeData =
-    (await codeExchangeResponse.json()) as GithubResponse;
+    (await codeExchangeResponse.json()) as CodeExchangeResponse;
   if ("error" in codeExchangeData) {
     if (codeExchangeData.error === "unverified_user_email") {
       throw new CustomHttpStatusError(
@@ -58,18 +72,40 @@ export async function getUserInfoGithub(requestBody: Oauth2RequestBody) {
   }
 
   const { access_token } = codeExchangeData;
+
   const userInfoResponse = await fetch("https://api.github.com/user", {
     headers: {
       Authorization: `bearer ${access_token}`,
+      Accept: "application/json",
     },
   });
-  // TODO: implement
   if (!userInfoResponse.ok) {
-    if (userInfoResponse.status >= 400 && userInfoResponse.status <= 499) {
-    }
+    throw new CustomHttpStatusError(502, "Failed to authenticate with Github.");
   }
+  const userInfoData = (await userInfoResponse.json()) as UserInfoResponse;
 
-  const userInfoData = await userInfoResponse.json();
+  const userEmailsResponse = await fetch("https://api.github.com/user/emails", {
+    headers: {
+      Authorization: `bearer ${access_token}`,
+      Accept: "application/json",
+    },
+  });
+  if (!userEmailsResponse.ok) {
+    throw new CustomHttpStatusError(502, "Failed to authenticate with Github.");
+  }
+  const userEmailsData =
+    (await userEmailsResponse.json()) as UserEmailsResponse;
+  const primaryEmail = userEmailsData.find((email) => {
+    return email.primary;
+  });
+  assert(primaryEmail, "Github primary email not found.");
 
-  console.log("User info data", userInfoData);
+  const userData: Oauth2UserData = {
+    email: primaryEmail.email,
+    name: userInfoData.name,
+    picture: userInfoData.avatar_url,
+    sub: String(userInfoData.id),
+  };
+
+  return userData;
 }
