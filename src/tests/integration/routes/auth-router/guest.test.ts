ebf12c836db341161ed679c6c548b5cc31ecbd5f
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-08-01 21:45:01 +0300

    test: initialize test file for signing in as a guest user

---
+++ src/tests/integration/routes/auth-router/guest.test.ts
@@ -0,0 +1,35 @@
+import { describe, expect, it } from "vitest";
+import express from "express";
+import request from "supertest";
+import indexRouter from "#src/routes/index-router.js";
+import type { AuthenticatedResponse } from "#src/types/auth.js";
+
+describe("guest sign in endpoint", () => {
+  const app = express();
+  app.use(indexRouter);
+
+  describe("signing in as guest POST /auth/guest", () => {
+    describe("request to endpoint", () => {
+      it("should return jwt token with guest user", async () => {
+        expect.hasAssertions();
+
+        const response = await request(app)
+          .post("/auth/guest")
+          .expect("Content-type", /json/)
+          .expect(200);
+
+        expect(response.body).toStrictEqual<AuthenticatedResponse>({
+          token: expect.any(String) as string,
+          user: {
+            id: expect.any(String) as string,
+            email: "guest-user",
+            fullName: "Guest",
+            isOnline: true,
+            picture: null,
+            provider: "local",
+          },
+        });
+      });
+    });
+  });
+});
