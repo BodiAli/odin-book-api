ea28225be99316c4d5396feb4605411e368228db
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-08-04 02:07:01 +0300

    Feat: create queries to get number of followers and followings and an array of followers

--- src/queries/followers-queries.ts
+++ src/queries/followers-queries.ts
@@ -1,6 +1,7 @@
 import prisma from "#src/db/prisma-client.js";
 import CustomHttpStatusError from "#src/errors/http-status-error.js";
 import { Prisma } from "#src/generated/prisma/client.js";
+import type { PublicUser } from "#src/types/users.js";
 
 export async function followUser(currentUserId: string, userId: string) {
   if (currentUserId === userId) {
@@ -41,4 +42,92 @@ export async function followUser(currentUserId: string, userId: string) {
 
     throw error;
   }
+}
+
+export async function getNumOfFollowers(userId: string) {
+  const doesUserExist = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
+
+  if (!doesUserExist) {
+    throw new CustomHttpStatusError(404, "User not found.");
+  }
+
+  const numberOfFollowers = await prisma.userFollow.count({
+    where: {
+      followingId: userId,
+    },
+  });
+
+  return numberOfFollowers;
+}
+
+export async function getNumOfFollowing(userId: string) {
+  const doesUserExist = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
+
+  if (!doesUserExist) {
+    throw new CustomHttpStatusError(404, "User not found.");
+  }
+
+  const numberOfFollowers = await prisma.userFollow.count({
+    where: {
+      followedById: userId,
+    },
+  });
+
+  return numberOfFollowers;
+}
+
+export async function getFollowers(userId: string): Promise<PublicUser[]> {
+  const doesUserExist = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+  });
+
+  if (!doesUserExist) {
+    throw new CustomHttpStatusError(404, "User not found.");
+  }
+
+  const userFollows = await prisma.userFollow.findMany({
+    where: {
+      followingId: userId,
+    },
+    include: {
+      followedBy: {
+        select: {
+          id: true,
+          fullName: true,
+          isOnline: true,
+          lastSeen: true,
+          profile: {
+            select: {
+              imageUrl: true,
+            },
+          },
+        },
+      },
+    },
+    orderBy: {
+      followedBy: {
+        fullName: "asc",
+      },
+    },
+  });
+
+  return userFollows.map((userFollow) => {
+    const { followedBy } = userFollow;
+    const { profile, ...follower } = followedBy;
+
+    return {
+      ...follower,
+      picture: profile ? profile.imageUrl : null,
+    };
+  });
 }
