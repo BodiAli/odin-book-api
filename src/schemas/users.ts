b2f45e60070fe046606520e5806c1527586acf3c
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-08-04 01:23:20 +0300

    Feat: create a PublicUser schema that will be displayed for all users

---
+++ src/schemas/users.ts
@@ -0,0 +1,28 @@
+import { z } from "zod";
+import { Provider } from "#src/generated/prisma/enums.js";
+import type { UserModel } from "#src/generated/prisma/models.js";
+
+export const user: z.ZodType<
+  Omit<UserModel, "password" | "lastSeen"> & { picture: string | null }
+> = z.object({
+  id: z.uuid(),
+  email: z.email(),
+  username: z.string(),
+  fullName: z.string(),
+  provider: z.enum(Provider),
+  picture: z.string().nullable(),
+  isOnline: z.boolean(),
+  isGuest: z.boolean(),
+});
+
+export const publicUser: z.ZodType<
+  Pick<UserModel, "id" | "fullName" | "isOnline" | "lastSeen"> & {
+    picture: string | null;
+  }
+> = z.object({
+  id: z.uuid(),
+  fullName: z.string(),
+  isOnline: z.boolean(),
+  lastSeen: z.date(),
+  picture: z.string().nullable(),
+});
