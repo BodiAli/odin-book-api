82bb1bb47b279f2d42d4316c9f192ef2aacea14a
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-08-03 23:34:21 +0300

    Feat: implment logic for handling errors and creating a follow relationship

--- src/queries/followers-queries.ts
+++ src/queries/followers-queries.ts
@@ -3,22 +3,38 @@ import CustomHttpStatusError from "#src/errors/http-status-error.js";
 import { Prisma } from "#src/generated/prisma/client.js";
 
 export async function followUser(currentUserId: string, userId: string) {
+  if (currentUserId === userId) {
+    throw new CustomHttpStatusError(400, "You cannot follow yourself.");
+  }
+
   try {
-    await prisma.user.update({
+    const followedByCurrentUser = await prisma.userFollow.findUnique({
       where: {
-        id: currentUserId,
-      },
-      data: {
-        following: {
-          connect: {
-            id: userId,
-          },
+        followedById_followingId: {
+          followedById: currentUserId,
+          followingId: userId,
         },
+      },
+      include: {
+        following: true,
+      },
+    });
+    if (followedByCurrentUser) {
+      throw new CustomHttpStatusError(
+        409,
+        `You already follow ${followedByCurrentUser.following.fullName}.`,
+      );
+    }
+
+    await prisma.userFollow.create({
+      data: {
+        followedById: currentUserId,
+        followingId: userId,
       },
     });
   } catch (error) {
     if (error instanceof Prisma.PrismaClientKnownRequestError) {
-      if (error.code === "P2025") {
+      if (error.code === "P2003") {
         throw new CustomHttpStatusError(404, "User not found.");
       }
     }
