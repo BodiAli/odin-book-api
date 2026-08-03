29c69c44d9395064910fc59b4fc2413b8e46dd6e
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-08-04 01:23:06 +0300

    Feat: create a PublicUser type that will be displayed for all users

--- src/types/users.ts
+++ src/types/users.ts
@@ -1,4 +1,5 @@
-import type { user } from "#src/schemas/user-schema.js";
+import type { publicUser, user } from "#src/schemas/users.js";
 import type { z } from "zod";
 
 export type User = z.infer<typeof user>;
+export type PublicUser = z.infer<typeof publicUser>;
