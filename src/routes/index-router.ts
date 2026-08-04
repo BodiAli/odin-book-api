dbce19be16c25a04490c2d3e2868182a9e047276
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-08-05 01:59:43 +0300

    Feat: mount usersRouter on /users path

--- src/routes/index-router.ts
+++ src/routes/index-router.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import authRouter from "./auth-router.js";
+import usersRouter from "./users-router.js";
 import "#src/config/passport.js";
 
 const indexRouter = express.Router();
 indexRouter.use(express.json());
 
 indexRouter.use("/auth", authRouter);
+indexRouter.use("/users", usersRouter);
 
 export default indexRouter;
