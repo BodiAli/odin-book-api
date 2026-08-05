bd2a23d2536f5c1e8b7e3030da2d05996003f21e
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-08-05 05:57:43 +0300

    Fix: use a custom middleware to send a custom error message when user is unauthorized

--- src/routes/index-router.ts
+++ src/routes/index-router.ts
@@ -1,5 +1,5 @@
-import express, { type RequestHandler } from "express";
-import passport from "passport";
+import express from "express";
+import authenticateJwt from "#src/middlewares/authenticate-jwt.js";
 import authRouter from "./auth-router.js";
 import usersRouter from "./users-router.js";
 import "#src/config/passport.js";
@@ -8,10 +8,6 @@ const indexRouter = express.Router();
 indexRouter.use(express.json());
 
 indexRouter.use("/auth", authRouter);
-indexRouter.use(
-  "/users",
-  passport.authenticate("jwt", { session: false }) as RequestHandler,
-  usersRouter,
-);
+indexRouter.use("/users", authenticateJwt, usersRouter);
 
 export default indexRouter;
