7fb2e2223cd9950046a0eeabe23d44d2e464b6e1
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-08-05 04:38:45 +0300

    Feat: authenticate the JWT for the /users route

--- src/routes/index-router.ts
+++ src/routes/index-router.ts
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { type RequestHandler } from "express";
+import passport from "passport";
 import authRouter from "./auth-router.js";
 import usersRouter from "./users-router.js";
 import "#src/config/passport.js";
@@ -7,6 +8,10 @@ const indexRouter = express.Router();
 indexRouter.use(express.json());
 
 indexRouter.use("/auth", authRouter);
-indexRouter.use("/users", usersRouter);
+indexRouter.use(
+  "/users",
+  passport.authenticate("jwt", { session: false }) as RequestHandler,
+  usersRouter,
+);
 
 export default indexRouter;
