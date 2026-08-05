cc13ba08883e7fe5196f29c27fb26070939f4eb1
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-08-05 05:54:15 +0300

    Test: fix test by providing Authorization with bearer scheme

--- src/tests/integration/routes/users-router/user-followers.test.ts
+++ src/tests/integration/routes/users-router/user-followers.test.ts
@@ -2,6 +2,8 @@ import { describe, expect, it } from "vitest";
 import request from "supertest";
 import express from "express";
 import indexRouter from "#src/routes/index-router.js";
+import prisma from "#src/db/prisma-client.js";
+import issueJwt from "#src/utils/issue-jwt.js";
 import type { ClientError } from "#src/types/errors.js";
 
 describe("user followers endpoints", () => {
@@ -13,9 +15,19 @@ describe("user followers endpoints", () => {
       it("should return 404 status with error message", async () => {
         expect.hasAssertions();
 
-        const response = await request(app).post(
-          "/users/nonExistingId/followers",
-        );
+        const currentUser = await prisma.user.create({
+          data: {
+            email: "test-email@test.com",
+            fullName: "test: full name",
+          },
+        });
+        const token = issueJwt(currentUser.id, "10m");
+
+        const response = await request(app)
+          .post("/users/nonExistingId/followers")
+          .auth(token, { type: "bearer" })
+          .expect("Content-type", /json/)
+          .expect(404);
 
         expect(response.body).toStrictEqual<ClientError>({
           errors: [
@@ -25,6 +37,10 @@ describe("user followers endpoints", () => {
           ],
         });
       });
+    });
+
+    describe("given valid target user id", () => {
+      it.todo("should follow target user");
     });
   });
 });
