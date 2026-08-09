c6a603207757bb9993a038b827d3576c7cbc7678
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-08-09 05:18:21 +0300

    Feat: create type for response of target user followings

--- src/types/routes/users.ts
+++ src/types/routes/users.ts
@@ -1,5 +1,6 @@
 import type {
   followersResponse,
+  followingsResponse,
   publicUser,
   user,
 } from "#src/schemas/routes/users.js";
@@ -9,3 +10,4 @@ export type User = z.infer<typeof user>;
 export type PublicUser = z.infer<typeof publicUser>;
 
 export type FollowersResponse = z.infer<typeof followersResponse>;
+export type FollowingsResponse = z.infer<typeof followingsResponse>;
