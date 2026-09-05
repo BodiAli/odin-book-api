920eb6d73a4686f0587e57c1738413f76f7e9e60
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-09-06 01:52:21 +0300

    Feat: integrate websocket to be used with the http server

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import "dotenv/config";
+import http from "node:http";
 import express from "express";
 import cors from "cors";
 import indexRouter from "./routes/index-router.js";
@@ -6,8 +7,12 @@ import openapiRouter from "./routes/openapi-router.js";
 import resourceNotFound from "./errors/resource-not-found.js";
 import errorHandler from "./errors/error-handler.js";
 import config from "./config/config.js";
+import webSocketApp from "./websocket/index.js";
 
 const app = express();
+const server = http.createServer(app);
+
+new webSocketApp().serverUpgrade(server);
 
 app.disable("x-powered-by");
 
@@ -19,6 +24,6 @@ app.use("/api-docs", openapiRouter);
 app.use(resourceNotFound);
 app.use(errorHandler);
 
-app.listen(config.port, () => {
+server.listen(config.port, () => {
   console.log(`Server listening on port ${String(config.port)}.`);
 });
