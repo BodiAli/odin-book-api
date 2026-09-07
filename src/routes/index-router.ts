77dae2c9dfcae5237d2350cb66e1bf0aa4ddf66f
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-09-07 08:11:22 +0300

    Feat: mount notifications router on /notifications path

--- src/routes/index-router.ts
+++ src/routes/index-router.ts
@@ -2,6 +2,7 @@ import express from "express";
 import authenticateJwt from "#src/middlewares/authenticate-jwt.js";
 import authRouter from "./auth-router.js";
 import usersRouter from "./users-router.js";
+import notificationsRouter from "./notifications-router.js";
 import "#src/config/passport.js";
 
 const indexRouter = express.Router();
@@ -9,5 +10,6 @@ indexRouter.use(express.json());
 
 indexRouter.use("/auth", authRouter);
 indexRouter.use("/users", authenticateJwt, usersRouter);
+indexRouter.use("/notifications", authenticateJwt, notificationsRouter);
 
 export default indexRouter;
