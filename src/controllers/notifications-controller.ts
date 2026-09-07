222b6b6c68539b2cde4988a85644cd04b6e44966
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-09-07 09:44:27 +0300

    Feat: implement retrieving current user notifications

--- src/controllers/notifications-controller.ts
+++ src/controllers/notifications-controller.ts
@@ -1,9 +1,16 @@
 import assert from "node:assert";
+import * as notificationQueries from "#src/queries/notification-queries.js";
 import type { Request, Response } from "express";
+import type { NotificationsResponse } from "#src/types/routes/notifications.js";
 
 export async function getCurrentUserNotifications(
   req: Request,
-  res: Response,
+  res: Response<NotificationsResponse>,
 ): Promise<void> {
   assert(req.user, "User not found");
+
+  const notifications = await notificationQueries.getUserNotifications(
+    req.user.id,
+  );
+  res.json({ notifications });
 }
