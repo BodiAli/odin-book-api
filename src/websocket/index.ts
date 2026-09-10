be1592a6be06c488ce842353a0c57d75d2a7516e
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-09-11 02:38:30 +0300

    Refactor: seprate adding listeners from handling events

--- src/websocket/index.ts
+++ src/websocket/index.ts
@@ -1,28 +1,33 @@
 import assert from "node:assert";
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 import authenticateUser from "./authenticate-user.js";
-import type { Server } from "node:http";
+import type { IncomingMessage, Server } from "node:http";
 
 class WebSocketApp {
   wss: WebSocketServer;
 
   constructor(server: Server) {
     this.wss = new WebSocketServer({ server });
-    this.handleConnection();
+    this.init();
   }
 
-  handleConnection(): void {
-    this.wss.on("connection", async (ws, req) => {
-      assert(req.url, "Url is not defined");
-      try {
-        const user = await authenticateUser(req.url);
-        ws.user = user;
-      } catch {
-        ws.close(1008, "Access token is missing or invalid.");
-        return;
-      }
-    });
+  private init(): void {
+    this.wss.on("connection", this.handleConnection);
   }
+
+  private handleConnection = async (
+    ws: WebSocket,
+    req: IncomingMessage,
+  ): Promise<void> => {
+    assert(req.url, "Url is not defined");
+    try {
+      const user = await authenticateUser(req.url);
+      ws.user = user;
+    } catch {
+      ws.close(1008, "Access token is missing or invalid.");
+      return;
+    }
+  };
 }
 
 export default WebSocketApp;
