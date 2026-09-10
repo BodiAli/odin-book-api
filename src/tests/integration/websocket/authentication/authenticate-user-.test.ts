703b4d6562346b5578d94eefee6a10d264a9ef1f
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-09-10 04:45:03 +0300

    Test: implement testing WebSocket authentication

--- src/tests/integration/websocket/authentication/authenticate-user-.test.ts
+++ src/tests/integration/websocket/authentication/authenticate-user-.test.ts
@@ -1,13 +1,38 @@
-import { createServer } from "node:http";
-import WebSocketApp from "#src/websocket/index.js";
+import WebSocket from "ws";
+import {
+  connectClient,
+  initiateWebSocketServer,
+  waitForClose,
+} from "#src/tests/setup/websocket-utils.js";
+import * as userQueries from "#src/queries/user-queries.js";
+import issueJwt from "#src/utils/issue-jwt.js";
 
 describe("authentication", () => {
-  const server = createServer();
-
   beforeAll(() => {
-    new WebSocketApp().serverUpgrade(server);
-    server.listen(3000);
+    initiateWebSocketServer();
   });
 
-  it.todo("should return a 401 response");
+  it("should return 1008 close code when token is invalid", async () => {
+    expect.hasAssertions();
+
+    const ws = new WebSocket("ws://localhost:8080?token=invalid-token");
+    const { code, reason } = await waitForClose(ws);
+
+    expect(code).toBe(1008);
+    expect(reason).toBe("Access token is missing or invalid.");
+  });
+
+  it("should accept connection when token is valid", async () => {
+    expect.hasAssertions();
+
+    const user = await userQueries.createUserLocal({
+      email: "test-email@test.com",
+      fullName: "test: full name",
+      password: "test-password",
+    });
+    const token = issueJwt(user.id, "10m");
+    const ws = await connectClient(token);
+
+    expect(ws.readyState).toBe(WebSocket.OPEN);
+  });
 });
