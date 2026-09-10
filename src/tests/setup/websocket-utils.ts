d9462c9a8933128166b064a926d232bf169bb1e0
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-09-10 04:45:39 +0300

    Feat: create utils for testing WebSocket server

--- src/tests/setup/websocket-utils.ts
+++ src/tests/setup/websocket-utils.ts
@@ -1,4 +1,6 @@
-import type { WebSocket } from "ws";
+import { createServer } from "node:http";
+import WebSocket from "ws";
+import WebSocketApp from "#src/websocket/index.js";
 
 export function waitForMessage<T>(ws: WebSocket): Promise<T> {
   return new Promise((resolve) => {
@@ -7,5 +9,31 @@ export function waitForMessage<T>(ws: WebSocket): Promise<T> {
       const data = JSON.parse(stringData) as T;
       resolve(data);
     });
+  });
+}
+
+export function waitForClose(
+  ws: WebSocket,
+): Promise<{ code: number; reason: string }> {
+  return new Promise((resolve) => {
+    ws.on("close", function (code, reason) {
+      resolve({ code, reason: reason.toString("utf-8") });
+    });
+  });
+}
+
+export function initiateWebSocketServer(): void {
+  const server = createServer();
+  new WebSocketApp(server);
+  server.listen(8080);
+}
+
+export function connectClient(token: string): Promise<WebSocket> {
+  return new Promise((resolve, reject) => {
+    const ws = new WebSocket(`ws://localhost:8080?token=${token}`);
+    ws.on("open", () => {
+      resolve(ws);
+    });
+    ws.on("error", reject);
   });
 }
