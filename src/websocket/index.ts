62f13fc0a20dc1fc3e14ecf9108358096ef05faa
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-09-10 04:49:54 +0300

    Fix: handle authentication during connection instead of handshake
    Browser’s WebSocket API has no way to get the http response code send when authentication during upgrade fails

--- src/websocket/index.ts
+++ src/websocket/index.ts
@@ -1,45 +1,26 @@
 import assert from "node:assert";
 import { WebSocketServer } from "ws";
 import authenticateUser from "./authenticate-user.js";
-import type { IncomingMessage, Server } from "node:http";
-import type { Duplex } from "node:stream";
+import type { Server } from "node:http";
 
 class WebSocketApp {
-  wss = new WebSocketServer({ noServer: true });
+  wss: WebSocketServer;
 
-  constructor() {
+  constructor(server: Server) {
+    this.wss = new WebSocketServer({ server });
     this.handleConnection();
   }
 
   handleConnection(): void {
-    this.wss.on("connection", (ws, req) => {
-      console.log("CONNECTED", req.user);
-    });
-  }
-
-  serverUpgrade(server: Server): void {
-    server.on("upgrade", (req, socket, head) => {
-      void this.handleUpgrade(req, socket, head);
-    });
-  }
-
-  private async handleUpgrade(
-    req: IncomingMessage,
-    socket: Duplex,
-    head: Buffer,
-  ): Promise<void> {
-    try {
-      assert(req.url, "req.url not defined");
-      const user = await authenticateUser(req.url);
-      req.user = user;
-    } catch {
-      socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
-      socket.destroy();
-      return;
-    }
-
-    this.wss.handleUpgrade(req, socket, head, (ws) => {
-      this.wss.emit("connection", ws, req);
+    this.wss.on("connection", async (ws, req) => {
+      assert(req.url, "Url is not defined");
+      try {
+        const user = await authenticateUser(req.url);
+        ws.user = user;
+      } catch {
+        ws.close(1008, "Access token is missing or invalid.");
+        return;
+      }
     });
   }
 }
