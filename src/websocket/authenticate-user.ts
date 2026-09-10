6ea7307a103a66c77736999e0504f80a87e8f2cf
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-09-10 04:47:33 +0300

    Style: group related logic together

--- src/websocket/authenticate-user.ts
+++ src/websocket/authenticate-user.ts
@@ -6,13 +6,12 @@ import type { User } from "#src/types/routes/users.js";
 
 export default async function authenticateUser(url: string): Promise<User> {
   const token = new URL(`${config.origin}${url}`).searchParams.get("token");
-
   if (!token) {
     throw new Error("Unauthorized");
   }
   const tokenPayload = jwt.verify(token, config.jwtSecret) as JwtPayload;
-
   assert(tokenPayload.sub, "Token sub is not defined");
+
   const user = await userQueries.getUserById(tokenPayload.sub);
   assert(user, "User not defined");
   return user;
