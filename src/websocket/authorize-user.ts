0f97a334502900ad9bef17fc1fd0a7a6024d87c1
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-09-11 06:15:38 +0300

    Fix: make function synchronous to make testing it easier

--- src/websocket/authorize-user.ts
+++ src/websocket/authorize-user.ts
@@ -1,10 +1,8 @@
 import assert from "node:assert";
 import jwt, { type JwtPayload } from "jsonwebtoken";
 import config from "#src/config/config.js";
-import * as userQueries from "#src/queries/user-queries.js";
-import type { User } from "#src/types/routes/users.js";
 
-export default async function authenticateUser(url: string): Promise<User> {
+export default function authorizeUser(url: string): string {
   const token = new URL(`${config.origin}${url}`).searchParams.get("token");
   if (!token) {
     throw new Error("Unauthorized");
@@ -12,7 +10,5 @@ export default async function authenticateUser(url: string): Promise<User> {
   const tokenPayload = jwt.verify(token, config.jwtSecret) as JwtPayload;
   assert(tokenPayload.sub, "Token sub is not defined");
 
-  const user = await userQueries.getUserById(tokenPayload.sub);
-  assert(user, "User not defined");
-  return user;
+  return tokenPayload.sub;
 }
