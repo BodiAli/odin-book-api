54019d69a4b7c16674dffba7c0b3765aa2fce6c9
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-09-11 04:16:08 +0300

    Refactor: change schema name to be more explicit

--- src/schemas/websocket/message-data.ts
+++ src/schemas/websocket/message-data.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import { EventType } from "#src/types/websocket/event-type.js";
 
-export const messageData = z.object({
+export const clientMessageData = z.object({
   type: z.enum(EventType, "Invalid event type."),
   data: z
     .string("Please provide a string data.")
--- src/websocket/validate-message.ts
+++ src/websocket/validate-message.ts
@@ -1,13 +1,13 @@
 import assert from "node:assert";
 import { ZodError } from "zod";
 import CustomWebSocketError from "#src/errors/websocket-error.js";
-import { messageData } from "#src/schemas/websocket/message-data.js";
+import { clientMessageData } from "#src/schemas/websocket/message-data.js";
 
 export default function validateMessage(data: Buffer): void {
   const stringData = data.toString("utf-8");
   try {
     const parsedData = JSON.parse(stringData) as object;
-    messageData.parse(parsedData);
+    clientMessageData.parse(parsedData);
   } catch (error) {
     if (error instanceof SyntaxError) {
       throw new CustomWebSocketError(1007, "Invalid JSON");
