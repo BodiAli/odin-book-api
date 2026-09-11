48a918e3c2dd01871f680e2a271b7338a8c099d1
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-09-11 04:14:10 +0300

    Feat: complete validating message data logic

--- src/websocket/validate-message.ts
+++ src/websocket/validate-message.ts
@@ -1,3 +1,4 @@
+import assert from "node:assert";
 import { ZodError } from "zod";
 import CustomWebSocketError from "#src/errors/websocket-error.js";
 import { messageData } from "#src/schemas/websocket/message-data.js";
@@ -5,16 +6,16 @@ import { messageData } from "#src/schemas/websocket/message-data.js";
 export default function validateMessage(data: Buffer): void {
   const stringData = data.toString("utf-8");
   try {
-    JSON.parse(stringData) as object;
-  } catch {
-    throw new CustomWebSocketError(1007, "Invalid JSON");
-  }
-
-  try {
-    messageData.parse(data);
+    const parsedData = JSON.parse(stringData) as object;
+    messageData.parse(parsedData);
   } catch (error) {
+    if (error instanceof SyntaxError) {
+      throw new CustomWebSocketError(1007, "Invalid JSON");
+    }
     if (error instanceof ZodError) {
-      throw new CustomWebSocketError(1007, error);
+      const firstIssue = error.issues[0];
+      assert(firstIssue, "First issue is undefined");
+      throw new CustomWebSocketError(1007, firstIssue.message);
     }
   }
 }
