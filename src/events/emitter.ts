50322c2ed7a9f63af8d1cfe06f129faadbc617b9
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-09-12 02:24:00 +0300

    Feat: create event emitter

---
+++ src/events/emitter.ts
@@ -0,0 +1,5 @@
+import EventEmitter from "node:events";
+
+const appEmitter = new EventEmitter();
+
+export default appEmitter;
---
+++ src/events/index.ts
@@ -0,0 +1,4 @@
+import appEmitter from "./emitter.js";
+import { EventType } from "./event-enum.js";
+
+export { appEmitter as emitter, EventType as events };
