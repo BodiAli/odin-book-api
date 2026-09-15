7badb4a152a4b75fe9b35824d72e7bf1845feb31
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-09-15 14:25:46 +0300

    Refactor: update import path and name

--- src/schemas/websocket/message-data.ts
+++ src/schemas/websocket/message-data.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
-import { events } from "#src/events/index.js";
+import { EventType } from "#src/types/websocket/event-type.js";
 
 export const clientMessageData = z.object({
-  type: z.enum(events, "Invalid event type."),
+  type: z.enum(EventType, "Invalid event type."),
   data: z
     .string("Please provide a string data.")
     .trim()
