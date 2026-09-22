bbd26b5d3868dd38489e9dae0729526e1cbb41b0
Abdulrahman Ali <150652018+BodiAli@users.noreply.github.com>
2026-09-22 11:02:45 +0300

    Test: implement testing hearbeat mechanism

--- tests/integration/websocket/heartbeat.test.ts
+++ tests/integration/websocket/heartbeat.test.ts
@@ -2,48 +2,127 @@ import Clients from "#src/websocket/clients.js";
 import {
   connectClient,
   initiateWebSocketServer,
+  waitForClose,
 } from "#test-utils/websocket-utils.js";
 import * as userQueries from "#src/queries/user-queries.js";
 import issueJwt from "#src/utils/issue-jwt.js";
-import type WebSocket from "ws";
+import type { User } from "#src/types/routes/users.js";
 
 describe("heartbeat mechanism", () => {
   beforeAll(() => {
     initiateWebSocketServer();
   });
 
+  let user: User;
+
+  beforeEach(async () => {
+    vi.useFakeTimers();
+    user = await userQueries.createUserLocal({
+      email: "test-email@test.com",
+      fullName: "test: full name",
+      password: "test-password",
+    });
+  });
+
   afterEach(() => {
+    vi.useRealTimers();
     const clientsInstance = Clients.getInstance();
     for (const client of clientsInstance.clients) {
       client.close();
     }
   });
 
-  function waitForPing(ws: WebSocket): Promise<string> {
-    return new Promise((resolve) => {
-      ws.on("ping", (data) => {
-        resolve(data.toString());
-      });
-    });
-  }
-
-  it("should send 'ping' message every 30 seconds", async () => {
+  it("should send ping client every 30 seconds", async () => {
     expect.hasAssertions();
 
-    const user = await userQueries.createUserLocal({
-      email: "test-email@test.com",
-      fullName: "test: full name",
-      password: "test-password",
-    });
     const token = issueJwt(user.id);
-    const ws = await connectClient(token);
+    const ws = await connectClient(token, false);
+    let pings = 0;
+    ws.on("ping", () => {
+      pings++;
+    });
 
-    vi.useFakeTimers();
-    const ping1 = await waitForPing(ws);
-    vi.advanceTimersByTime(30000);
-    const ping2 = await waitForPing(ws);
+    // use async to wait for asynchronous code to execute
+    // first iteration
+    await vi.advanceTimersToNextTimerAsync();
+    ws.pong();
+    // second iteration
+    await vi.advanceTimersToNextTimerAsync();
 
-    expect(ping1).toBe("ping");
-    expect(ping2).toBe("ping");
+    expect(pings).toBe(2);
+  });
+
+  it("should remove client when client doesn't respond to the ping", async () => {
+    expect.hasAssertions();
+
+    const clientsInstance = Clients.getInstance();
+    const token = issueJwt(user.id);
+    await connectClient(token, false);
+
+    await vi.runOnlyPendingTimersAsync();
+    const userClients = clientsInstance.clients;
+    const isUserConnected = clientsInstance.hasConnection(user.id);
+
+    expect(userClients).toHaveLength(0);
+    expect(isUserConnected).toBe(false);
+  });
+
+  it("should remove all clients that are not responding", async () => {
+    expect.hasAssertions();
+
+    const clientsInstance = Clients.getInstance();
+    const token = issueJwt(user.id);
+    await connectClient(token, false);
+    await connectClient(token, false);
+
+    await vi.runOnlyPendingTimersAsync();
+    const userClients = clientsInstance.clients;
+    const isUserConnected = clientsInstance.hasConnection(user.id);
+
+    expect(userClients).toHaveLength(0);
+    expect(isUserConnected).toBe(false);
+  });
+
+  it("should not remove client when client responds to ping with pong", async () => {
+    expect.hasAssertions();
+
+    const clientsInstance = Clients.getInstance();
+    const token = issueJwt(user.id);
+    const ws = await connectClient(token, false);
+
+    // first iteration
+    await vi.advanceTimersToNextTimerAsync();
+    ws.pong();
+    // second iteration
+    await vi.advanceTimersToNextTimerAsync();
+    const userClients = clientsInstance.clients;
+    const isUserConnected = clientsInstance.hasConnection(user.id);
+
+    expect(userClients).toHaveLength(1);
+    expect(isUserConnected).toBe(true);
+  });
+
+  it("should only remove the client that is not responding", async () => {
+    expect.hasAssertions();
+
+    const clientsInstance = Clients.getInstance();
+    const token = issueJwt(user.id);
+    const ws1 = await connectClient(token, false);
+    const ws2 = await connectClient(token, false);
+
+    // first iteration
+    await vi.advanceTimersToNextTimerAsync();
+    ws1.pong();
+    // second iteration
+    await vi.advanceTimersToNextTimerAsync();
+    // use real timers so the timer in waitForClose will run
+    vi.useRealTimers();
+    const { code } = await waitForClose(ws2);
+    const userClients = clientsInstance.clients;
+    const isUserConnected = clientsInstance.hasConnection(user.id);
+
+    expect(userClients).toHaveLength(1);
+    expect(isUserConnected).toBe(true);
+    expect(code).toBe(1006);
   });
 });
